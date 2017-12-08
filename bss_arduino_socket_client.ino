0c79d8d04e99afb65c5791f5a2c0f1b784e7bf5d
Nhan Cao <nhancv92@gmail.com>
2017-12-08 17:54:11 +0700

    simple webserver

--- bss_arduino_socket_client.ino
+++ bss_arduino_socket_client.ino
@@ -2,7 +2,55 @@
 #include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
 #include "images.h"
 
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#define ON 0 
+#define OFF 1
+const char* ssid = "anonymous357";
+const char* password = "_n@1234567980";
+
+ESP8266WebServer server(80);
+
 SSD1306  display(0x3c, 4, 5);
+
+
+const int led = 16;
+
+void handleRoot() {
+  digitalWrite(led, OFF);
+  server.send(200, "text/plain", "hello from esp8266!");
+  digitalWrite(led, ON);
+}
+
+void handleNotFound(){
+  digitalWrite(led, OFF);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, ON);
+}
+
+void drawText(String text){
+  display.clear();
+  display.drawStringMaxWidth(0, 0, 128, text);
+  display.display();
+}
+
+void drawString(int16_t x, int16_t y, String text){
+  display.drawString(x, y, text);
+  display.display();
+}
 
 void setup() {
   Serial.begin(115200);
@@ -12,23 +60,54 @@ void setup() {
   display.init();
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
-
-}
-
-void drawText(String text){
-  display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawStringMaxWidth(0, 0, 128, text);
+  
+  display.clear();
+  drawString(0, 0, "Beesight Soft");
+  
+  pinMode(led, OUTPUT);
+  
+  WiFi.begin(ssid, password);
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(led, !digitalRead(led));
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected to: ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Display info on LCD
+  drawString(0, 10, String("SSID: " + String(ssid)));
+  drawString(0, 20, String("IP: " + WiFi.localIP().toString()));
+  
+  if (MDNS.begin("esp8266")) {
+    drawString(0, 30, "MDNS responder started");
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", [](){
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  digitalWrite(led, ON);
+  drawString(0, 40, "HTTP server started");
+  Serial.println("HTTP server started");
+  
 }
 
 void loop() {
-  // clear the display
-  display.clear();
 
-  drawText("Hello everybody");
+  server.handleClient();
   
-  // write the buffer to the display
-  display.display();
-
   delay(10);
 }
