a5c0f9fce29e0e78766288caa04ac5e28be635d0
Nhan Cao <nhancv92@gmail.com>
2017-12-11 16:40:04 +0700

    update socket
    esp open socket server(81): listen config of host
    
    nodejs open socket server (3000)
    nodejs send config to esp {"host":"192.168.0.101","port":3000,"path":"/"}
    
    esp read config and connect to nodejs
    esp close socket server

--- bss_arduino_socket.ino
+++ bss_arduino_socket.ino
@@ -15,66 +15,84 @@ const char* password = "_n@1234567980";
 
 SSD1306  display(0x3c, 4, 5);
 
-WebSocketsServer wss = WebSocketsServer(81);
-WebSocketsClient webSocket;
+WebSocketsServer socketServer = WebSocketsServer(81);
+WebSocketsClient socketClient;
 
-bool socketClientRequest = false;
+bool socketServerStopped = false;
 bool socketClientInit = false;
 
 const int led = 16;
 
-JsonObject& parseJSON(String json) {
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(json);
-  if (!root.success()) {
-    Serial.println("parseObject() failed");
-  }
-  return root;
-}
-
-void drawText(String text){
+void drawText(String text) {
   display.clear();
   display.drawStringMaxWidth(0, 0, 128, text);
   display.display();
 }
 
-void drawString(int16_t x, int16_t y, String text){
+void drawString(int16_t x, int16_t y, String text) {
   display.drawString(x, y, text);
   display.display();
 }
 
+void setupSocketClient(String host, uint16_t port, String path = "/") {
+  // server address, port and URL
+  socketClient.begin(host, port, path);
+  // event handler
+  socketClient.onEvent(webSocketClientEvent);
+  // try ever 5000 again if connection has failed
+  socketClient.setReconnectInterval(5000);
+
+  socketServer.disconnect();
+  socketServerStopped = true;
+  socketClientInit = true;
+}
+
+void socketServerLoop() {
+  if(!socketServerStopped) {
+    socketServer.loop();    
+  }
+}
+
+void socketClientLoop() {
+  if(socketClientInit) {
+    socketClient.loop();  
+  }
+}
+
 void webSocketServerEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
-
-    switch(type) {
-        case WStype_DISCONNECTED:
-            Serial.printf("[%u] Disconnected!\n", num);
-            break;
-        case WStype_CONNECTED:
-            {
-                IPAddress ip = wss.remoteIP(num);
-                Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-        
-                // send message to client
-                wss.sendTXT(num, "Connected");
+  StaticJsonBuffer<200> jsonBuffer;
+  switch(type) {
+      case WStype_DISCONNECTED:
+          Serial.printf("[%u] Disconnected!\n", num);
+          break;
+      case WStype_CONNECTED:
+          {
+            IPAddress ip = socketServer.remoteIP(num);
+            Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+            drawString(0, 50, String("Client connected"));
+          }
+          break;
+      case WStype_TEXT:
+          {
+            //String config = "{\"host\":\"192.168.0.101\",\"port\":3000,\"path\":\"/\"}";
+            JsonObject& root = jsonBuffer.parseObject((char*) payload);
+            if (!root.success()) {
+              Serial.println("parseObject() failed");
+              break;
             }
-            break;
-        case WStype_TEXT:
-            Serial.printf("[%u] get Text: %s\n", num, payload);
-
+            
+            const char* host = root["host"];
+            int16_t port = root["port"];
+            const char* path = root["path"];
+  
+            setupSocketClient (host, port, path);
+            Serial.printf("[%u] get Config: %s:%d%s\n", num, host, port, path);
+       
             // send message to client
-            // webSocket.sendTXT(num, "message here");
-
-            // send data to all connected clients
-            // webSocket.broadcastTXT("message here");
-            break;
-        case WStype_BIN:
-            Serial.printf("[%u] get binary length: %u\n", num, length);
-            hexdump(payload, length);
-
-            // send message to client
-            // webSocket.sendBIN(num, payload, length);
-            break;
-    }
+            socketServer.sendTXT(num, "{\"esp\":\"done\"}");
+          }
+          break;
+  }
 }
 
 void webSocketClientEvent(WStype_t type, uint8_t * payload, size_t length) {
@@ -83,45 +101,40 @@ void webSocketClientEvent(WStype_t type, uint8_t * payload, size_t length) {
     case WStype_DISCONNECTED:
       Serial.printf("[WSc] Disconnected!\n");
       break;
-    case WStype_CONNECTED: {
-      Serial.printf("[WSc] Connected to url: %s\n", payload);
-
-      // send message to server when Connected
-      webSocket.sendTXT("Connected");
-    }
+    case WStype_CONNECTED: 
+      {
+        Serial.printf("[WSc] Connected to url: %s\n", payload);
+        drawString(0, 50, String("Connected to server"));
+      }
       break;
     case WStype_TEXT:
-      Serial.printf("[WSc] get text: %s\n", payload);
+      {
+        Serial.printf("[WSc] get text: %s\n", payload);
 
-      // send message to server
-      // webSocket.sendTXT("message here");
+        char * command = (char*) payload;
+        
+        drawString(0, 40, String(command));
+        
+        if (strcmp(command, "W") == 0){
+            
+        } else if (strcmp(command, "A") == 0){
+            
+        } else if (strcmp(command, "D") == 0){
+            
+        } else if (strcmp(command, "S") == 0){
+            
+        }
+        
+        // send message to server
+        // socketClient.sendTXT("message here");
+      }
       break;
-    case WStype_BIN:
-      Serial.printf("[WSc] get binary length: %u\n", length);
-      hexdump(payload, length);
-
-      // send data to server
-      // webSocket.sendBIN(payload, length);
-      break;
-  }
-}
-
-void setupSocketClient(String host, uint16_t port, String url = "/") {
-  if(socketClientRequest) {
-    if(!socketClientInit) {
-      // server address, port and URL
-      webSocket.begin(address, port, url);
-      // event handler
-      webSocket.onEvent(webSocketClientEvent);
-      // try ever 5000 again if connection has failed
-      webSocket.setReconnectInterval(5000);
-    } else {
-      webSocket.loop();  
-    }
   }
 }
 
 void setup() {
+  pinMode(led, OUTPUT);
+  
   Serial.begin(115200);
   Serial.println();
 
@@ -134,39 +147,32 @@ void setup() {
   display.clear();
   drawString(0, 0, "Beesight Soft");
   
-  pinMode(led, OUTPUT);
-  
   WiFi.begin(ssid, password);
-
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     digitalWrite(led, !digitalRead(led));
     delay(500);
     Serial.print(".");
   }
-  Serial.println();
-  Serial.print("Connected to: ");
-  Serial.println(ssid);
+  Serial.printf("\nConnected to: %s\n", ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
+  
   // Display info on LCD
   drawString(0, 10, String("SSID: " + String(ssid)));
   drawString(0, 20, String("IP: " + WiFi.localIP().toString()));
 
   digitalWrite(led, ON);
 
-  wss.begin();
-  wss.onEvent(webSocketServerEvent);
+  socketServer.begin();
+  socketServer.onEvent(webSocketServerEvent);
 
 }
 
 void loop() {
-
-  server.handleClient();
-
-  wss.loop();
+  socketServerLoop();
   
-
+  socketClientLoop();
+  
   delay(10);
 }
